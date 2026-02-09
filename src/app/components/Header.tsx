8d3ebaaaee6f3e4d47f38e87452c8ee699603c3b
Mokki78 <100276996+Mokki78@users.noreply.github.com>
2026-02-09 13:23:56 +0100

    feat: add site header  with navigation

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Header() {
-    return <h1>Hero</h1>
+    return (
+        <header>
+            <nav>
+                <Link href="/">Home</Link>
+                <Link href="/om">Om oss</Link>
+                <Link href="/kontakt">Kontakt</Link>
+            </nav>
+        </header>
+    )
 }
