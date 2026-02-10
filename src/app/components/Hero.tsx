8f45f5ac488cc9e81d498f10639496ecac919f87
Mokki78 <100276996+Mokki78@users.noreply.github.com>
2026-02-10 08:59:42 +0100

    feat: add images nad hero image

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,6 +4,7 @@ export default function Hero() {
       <section>
         <h2>Hero</h2>
         <p>Your body is your canvas and we help you get the art you diserve.</p>
+        <img src="../images/AdobeStock_925090751_Preview.jpeg" className="heroimg"/>
         <button className="bookingBtn">Booking</button>
       </section>
     </>
---
+++ src/app/services/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+export default function Tjenester() {
+  return (
+    <>
+      <section>
+        <Header />
+        <h2>Våre tjenester</h2>
+        <p>Blablabla</p>
+      </section>
+    </>
+  );
+}
