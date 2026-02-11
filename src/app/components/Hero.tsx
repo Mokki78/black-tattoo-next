314365eb223a68d1e906662677860ab9d85d9c87
Mokki78 <100276996+Mokki78@users.noreply.github.com>
2026-02-11 13:49:40 +0100

    feat: add styling to main nav and CTA button

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <>
       <section>
-        <h2>Hero</h2>
+        
         <p>Your body is your canvas and we help you get the art you diserve.</p>
         <img src="../images/AdobeStock_925090751_Preview.jpeg" className="heroimg"/>
         <button className="bookingBtn">Booking</button>
