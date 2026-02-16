736c7a2dc4b1e75cf4c25c5eea84d134448f6ca0
Mokki78 <100276996+Mokki78@users.noreply.github.com>
2026-02-16 11:03:11 +0100

    feat: add gallery comp, remove photos and add new

---
+++ src/app/components/Gallery.tsx
@@ -0,0 +1,12 @@
+export default function Gallery() {
+  return (
+    <section className="ImgGallery">
+      <img src="../AdobeStock_110762807_Preview.jpeg" className="galleryImg" />
+      <img src="../AdobeStock_224671539_Preview.jpeg" className="galleryImg" />
+      <img src="../AdobeStock_291190864_Preview.jpeg" className="galleryImg" />
+      <img src="../AdobeStock_479783906_Preview.jpeg" className="galleryImg" />
+      <img src="../AdobeStock_840181486_Preview.jpeg" className="galleryImg" />
+      <img src="../AdobeStock_1146516039_Preview.jpeg" className="galleryImg" />
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Gallery from "./components/Gallery";
 import Services from "./components/Services";
 import Footer from "./components/Footer";
+
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Gallery />
       <Services />
       <Footer />
     </>
